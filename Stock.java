5aa5b968584080316f68e56bfe6a1154cf605504
prashant5harma <mr.prashansharma@gmail.com>
2017-02-22 11:48:58 -0500

    graph

--- Stock.java
+++ Stock.java
@@ -1,3 +1,6 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 /**
  * Created by Silas on 2017/2/20.
  */
@@ -11,9 +14,16 @@ public class Stock {
         String[] s1 = new String[7];
         s1 = s.split("[,]");
         date = s1[0];
+        System.out.print(date + " ");
+        
         for (int i=0; i<6; i++){
             data[i]  = Double.parseDouble(s1[i+1]);
+           
+            System.out.print(data[i]+ " ");
         }
-
+        
+        
+        
+        System.out.println();
     }
 }
--- main.java
+++ main.java
@@ -1,6 +1,13 @@
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  * Created by Silas on 2017/2/20.
@@ -8,15 +15,26 @@ import java.io.IOException;
 public class main {
 
     public static void main(String[] args) throws IOException{
-        FileReader fr = new FileReader("E:\\workspace\\readCSV\\src\\stockdata.csv");
+        FileReader fr = new FileReader("/Users/prashantsharma/Documents/workspace/Stock/src/Sampledata.csv");
         BufferedReader br1 = new BufferedReader(fr);
         String s = new String();
         int i =0;
         Stock[] s1 = new Stock[14000];
         s = br1.readLine();
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        
         while((s = br1.readLine())!=null) {
             s1[i] = new Stock(s);
+            dataset.setValue(s1[i].data[3], "Stock Price", s1[i].date);
             i++;
         }
+        JFreeChart chart=ChartFactory.createLineChart("Stock Analysis", "Time", "Price", dataset);
+        		chart.setBackgroundPaint(Color.white);
+        		chart.getTitle().setPaint(Color.red);
+        		CategoryPlot p = chart.getCategoryPlot();
+        		p.setRangeMinorGridlinePaint(Color.blue);
+        		ChartFrame frame = new ChartFrame("Line Chart",chart);
+        		frame.setVisible(true);
+        		frame.setSize(450,350);
     }
 }
